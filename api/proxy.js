75bc913c9adcf506c6a2a7aa7f6564f3acbeb06b
starkesrl-cloud <stark.esrl@gmail.com>
2026-04-24 16:47:34 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -29,9 +29,10 @@ export default async function handler(req, res) {
     
     try {
       const data = JSON.parse(text);
-if (data && data.objects) {
-  data.objects = data.objects.filter(p => parseInt(p.qt_stock, 10) !== 1);
+if (data && data.data) {
+  data.data = data.data.filter(p => parseInt(p.qt_stock, 10) !== 1);
 }
+
 res.status(response.status).json(data);
 
     } catch {
